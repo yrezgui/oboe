b4c34ce235b36e96e6b4a51a07a687bf218f1c25
Phil Burk <philburk@mobileer.com>
2021-06-17 22:42:06 -0700

    OboeTester: add external tap-to-tone

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/ExternalTapToToneActivity.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/ExternalTapToToneActivity.java
@@ -1,16 +1,45 @@
 package com.mobileer.oboetester;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
-public class ExternalTapToToneActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 2345;
-    private TextView mResultView;
+import java.io.IOException;
+
+public class ExternalTapToToneActivity extends Activity {
+    protected TapToToneTester mTapToToneTester;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_tap_to_tone);
+
+        mTapToToneTester = new TapToToneTester(this);
+    }
+
+    public void analyseAndShowResults() {
+        TapToToneTester.TestResult result = mTapToToneTester.analyzeCapturedAudio();
+        if (result != null) {
+            mTapToToneTester.showTestResults(result);
+        }
+    }
+
+    public void analyze(View view) {
+        analyseAndShowResults();
+    }
+
+    public void startTest(View view) throws IOException {
+        mTapToToneTester.resetLatency();
+        mTapToToneTester.start();
+    }
+
+    public void stopTest(View view) {
+        mTapToToneTester.stop();
+    }
+
+    @Override
+    public void onStop() {
+        mTapToToneTester.stop();
+        super.onStop();
     }
 }
